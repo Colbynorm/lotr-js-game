0312a2e00281855d76e4bd363a4f64bfd2b6b3e1
colby <colbynorm41@gmail.com>
2021-03-15 12:21:12 -0500

    added to game

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,10 +1,10 @@
 let GameManager = {
-    setGameStart: function(classType) {
+    setGameStart: function (classType) {
         this.resetPlayer(classType);
         this.setPreFight();
     },
-    resetPlayer: function(classType) {
-        switch(classType) {
+    resetPlayer: function (classType) {
+        switch (classType) {
             case "Aragorn":
                 player = new Player(classType, 200, 0, 200, 100, 50);
                 break;
@@ -19,15 +19,38 @@ let GameManager = {
                 break;
         }
         let getInterface = document.querySelector('.interface');
-        getInterface.innerHTML = '<img src="gamePics/' + classType.toLowerCase() + '.jpg" class="img-avatar"><div><h3>' + classType + '</h3> <p>Health: '+ player.health +'</p> <p>Mana: '+ player.mana +'</p> <p>Strength: '+ player.strength +'</p> <p>Agility: '+ player.agility +'</p> <p>Speed: '+ player.speed +'</p></div>';
+        getInterface.innerHTML = '<img src="gamePics/' + classType.toLowerCase() + '.jpg" class="img-avatar"><div><h3>' + classType + '</h3> <p>Health: ' + player.health + '</p> <p>Mana: ' + player.mana + '</p> <p>Strength: ' + player.strength + '</p> <p>Agility: ' + player.agility + '</p> <p>Speed: ' + player.speed + '</p></div>';
     },
-    setPreFight: function() {
+    setPreFight: function () {
         let getHeader = document.querySelector(".header");
         let getActions = document.querySelector(".actions");
         let getArena = document.querySelector(".arena");
 
         getHeader.innerHTML = '<p>Task: Find an enemy!</p>';
         getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for enemy!</a>';
-        getArena.
+        getArena.style.visibility = "visible";
+    },
+    setFight: function () {
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
+
+        //Create Enemy!
+        let enemy00 = new Enemy("Sauron", 100, 0, 50, 100, 100);
+        let enemy01 = new Enemy("Witch King Of Angmar", 200, 0, 150, 80, 150);
+
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+
+        switch (chooseRandomEnemy) {
+            case 0:
+                enemy = enemy00;
+                break;
+            case 1:
+                enemy = enemy01;
+                break;
+        }
+        getHeader.innerHTML = '<p>Task: Choose Your Move!</p>';
+        getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Attack!</a>';
+        getEnemy.innerHTML = '<img src="gamePics/' + enemy.enemyType.toLowerCase() + '.jpg" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Stength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
     }
 }
